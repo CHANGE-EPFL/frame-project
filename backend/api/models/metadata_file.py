083b3fabeaafaa0704cc35ae81f4611c75fda22e
Son Pham-Ba <son.phamba@epfl.ch>
2024-12-17 00:32:43 +0100

    feat: add json schema for yaml

--- backend/api/models/metadata_file.py
+++ backend/api/models/metadata_file.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic import BaseModel
 
 from .data import DataIO
@@ -13,3 +15,11 @@ class MetadataFromFile(BaseModel, extra="forbid"):
     physics_based_components: list[PhysicsBasedComponentFromFile] = []
     machine_learning_components: list[MachineLearningComponentFromFile] = []
     data: DataIO = DataIO()
+
+
+def export_json_schema(output_path: str) -> None:
+    """Export JSON schema for MetadataFromFile."""
+    json_schema = MetadataFromFile.model_json_schema()
+
+    with open(output_path, "w") as file:
+        json.dump(json_schema, file, indent=4)
