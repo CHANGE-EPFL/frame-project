c919bf3ff06ef632814eb4f7055e3d2a81415070
Son Pham-Ba <son.phamba@epfl.ch>
2024-12-17 13:47:22 +0100

    chore(frontend): use quasar native copytoclipboard

--- frontend/src/components/PullCommand.vue
+++ frontend/src/components/PullCommand.vue
@@ -9,11 +9,7 @@
       class="pull-command q-pa-none"
     >
       <template v-slot:append>
-        <q-icon
-          name="content_copy"
-          @click="copyToClipboard"
-          class="cursor-pointer"
-        />
+        <q-icon name="content_copy" @click="copy" class="cursor-pointer" />
       </template>
     </q-input>
   </div>
@@ -21,7 +17,7 @@
 
 <script setup lang="ts">
 import { defineProps, ref } from 'vue';
-import { useQuasar } from 'quasar';
+import { copyToClipboard, Notify } from 'quasar';
 
 const props = defineProps({
   type: {
@@ -35,26 +31,25 @@ const props = defineProps({
 });
 
 const inputText = ref(`frame pull ${props.type} ${props.short_name}`);
-const $q = useQuasar();
 
-// Method to copy text to the clipboard
-const copyToClipboard = () => {
-  // Create a temporary text area to help with copying text to clipboard
-  const textArea = document.createElement('textarea');
-  textArea.value = inputText.value;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand('copy');
-  document.body.removeChild(textArea);
-
-  $q.notify({
-    type: 'positive',
-    message: 'Command copied to clipboard',
-  });
-};
+function copy() {
+  copyToClipboard(inputText.value)
+    .then(() => {
+      Notify.create({
+        message: 'Copied to clipboard',
+        color: 'positive',
+      });
+    })
+    .catch(() => {
+      Notify.create({
+        message: 'Failed to copy to clipboard',
+        color: 'negative',
+      });
+    });
+}
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .pull-command {
   font-family: monospace;
   font-size: 0.9em;
