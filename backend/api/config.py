a083d9cd8ec6240f02757aa03b04d8dedaa10112
Son Pham-Ba <son.phamba@epfl.ch>
2025-01-21 10:19:28 +0100

    fix(backend): remove unused config parameters

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,22 +1,10 @@
 from functools import lru_cache
 
-from pydantic import model_validator
 from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-
-    API_KEYS: str
-
     PATH_PREFIX: str = "/api"
-
-    S3_ENDPOINT_PROTOCOL: str
-    S3_ENDPOINT_HOSTNAME: str
-    S3_ACCESS_KEY_ID: str
-    S3_SECRET_ACCESS_KEY: str
-    S3_REGION: str
-    S3_BUCKET: str
-    S3_PATH_PREFIX: str
 
 
 @lru_cache()
