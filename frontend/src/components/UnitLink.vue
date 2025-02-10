1a0cf8147f6f2db4ee914c052fca67d1582225fe
Son Pham-Ba <son.phamba@epfl.ch>
2025-02-10 11:59:31 +0100

    fix(frontend): wrong unit link href (missing #)

--- frontend/src/components/UnitLink.vue
+++ frontend/src/components/UnitLink.vue
@@ -1,7 +1,9 @@
 <template>
   <span>
     <q-icon :name="iconName" class="q-mr-xs" />
-    <a :href="`/${unitType}/${unit.id}`" :class="linkClass">{{ unit.name }}</a>
+    <a :href="`/#/${unitType}/${unit.id}`" :class="linkClass">{{
+      unit.name
+    }}</a>
   </span>
 </template>
 
