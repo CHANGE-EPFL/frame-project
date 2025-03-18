6068fa08c1eb435e48b28a440a8c2587672f7b38
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-18 16:52:37 +0100

    feat: update cli installation instructions

--- frontend/src/pages/CliPage.vue
+++ frontend/src/pages/CliPage.vue
@@ -5,28 +5,31 @@
 
       <p>
         To download models and components, and to setup environments to run
-        those, your can use the dedicated CLI (command-line interface) tool.
+        those, your can use the dedicated CLI (command-line interface)
+        <a href="https://github.com/CHANGE-EPFL/frame-project-cli">tool</a>.
       </p>
 
       <h2>Installation</h2>
       <p>
-        <i><u>Requirements</u>: Python 3.10 or higher.</i>
+        <i
+          ><u>Requirements</u>:
+          <a href="https://docs.astral.sh/uv/">uv</a> Python package and project
+          manager.</i
+        >
       </p>
       <p>
-        We recommend you install the FRAME CLI inside a Python virtual
-        environment, for example using
-        <a href="https://github.com/astral-sh/uv">uv</a> or
-        <a
-          href="https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html"
-          >conda</a
-        >. To install, run the following command in your terminal:
+        Frame CLI relies on <a href="https://docs.astral.sh/uv/">uv</a> to
+        manage Python virtual environments. You need to install it first if you
+        don't already have it:
       </p>
+      <CopyCommand command="wget -qO- https://astral.sh/uv/install.sh | sh" />
+      <p>Then, run the following command to install Frame CLI:</p>
       <CopyCommand
-        command="pip install git+https://gitlab.com/CHANGE-EPFL/frame-project-cli.git"
+        command="uv tool install git+https://github.com/CHANGE-EPFL/frame-project-cli.git"
       />
 
       <h2>Usage</h2>
-      <p>To get hints on how to use the CLI tool, run:</p>
+      <p>To see the list of available commands, run:</p>
       <CopyCommand command="frame-cli --help" />
       <p>
         Hybrid model and component pages show which command must be run to
