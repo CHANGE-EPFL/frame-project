e86ab90071dda7f7fbcb129d95aea0fc23524c35
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-21 10:08:44 +0100

    feat: model id must be provided in the metadata file

--- backend/api/models/component.py
+++ backend/api/models/component.py
@@ -6,13 +6,9 @@ from .common_metadata import CommonMetadataIncomplete, CommonMetadataSummary
 class ComponentFromFile(CommonMetadataIncomplete):
     """Component of hybrid model."""
 
-    id: str
-
 
 class ComponentSummary(CommonMetadataSummary, extra="ignore"):
     """Essential metadata fields for hybrid model components."""
-
-    id: str
 
 
 class Component(ComponentFromFile, ComponentSummary):
