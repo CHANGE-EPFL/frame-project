e2ab14738db3bdd061df24d1062092a8923750da
Son Pham-Ba <son.phamba@epfl.ch>
2025-05-02 14:39:10 +0200

    feat: add automatic yaml metadata template generation

--- backend/api/models/metadata_file.py
+++ backend/api/models/metadata_file.py
@@ -1,4 +1,7 @@
+import inspect
 import json
+import types
+import typing
 
 from pydantic import BaseModel
 
@@ -23,3 +26,77 @@ def export_json_schema(output_path: str) -> None:
 
     with open(output_path, "w") as file:
         json.dump(json_schema, file, indent=4)
+
+
+def export_yaml_template(output_path: str) -> None:
+    """Export YAML template for MetadataFromFile."""
+
+    template = (
+        "# yaml-language-server: $schema=https://raw.githubusercontent.com/CHANGE-EPFL/frame-project/"
+        "refs/heads/dev/backend/api/metadata_files/schema.json"
+    )
+    template += get_annotation_template(MetadataFromFile)
+
+    with open(output_path, "w") as file:
+        file.write(template)
+
+
+def get_annotation_template(model, current_level: int = 0, optional: bool = False) -> str:
+    template = ""
+    indent = "  " * current_level
+
+    if isinstance(model, types.UnionType):
+        models = list(typing.get_args(model))
+        if type(None) in models:
+            models.remove(type(None))
+
+        if len(models) == 1:
+            model = models[0]
+            optional = True
+
+        else:
+            template += f" # {' | '.join([m.__name__ for m in models])}"
+            if optional:
+                template += ", optional"
+            return template
+
+    if model is str:
+        template += ' "" # string'
+        if optional:
+            template += ", optional"
+        return template
+
+    if model is bool:
+        template += " true # boolean"
+        if optional:
+            template += ", optional"
+        return template
+
+    if isinstance(model, types.GenericAlias) or isinstance(model, typing._GenericAlias):
+        if typing.get_origin(model) is list:
+            template += " # list"
+            if optional:
+                template += ", optional"
+            template += f"\n{indent}- "
+            template += get_annotation_template(typing.get_args(model)[0], current_level + 1).lstrip()
+        return template
+
+    if not isinstance(model, types.GenericAlias) and inspect.isclass(model) and issubclass(model, BaseModel):
+        if optional:
+            template += " # optional"
+
+        for name, field in model.model_fields.items():
+            if current_level == 0:
+                template += "\n"
+
+            template += f"\n{indent}{name}:"
+            template += get_annotation_template(field.annotation, current_level + 1, optional=not field.is_required())
+
+        return template
+
+    template += f" # {model.__name__}"
+    if optional:
+        template += ", optional"
+    print(f"Unsupported type: {model}.")
+    return template
+    # raise TypeError(f"Unsupported type: {model}.")
