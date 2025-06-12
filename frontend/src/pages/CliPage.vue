d1e4af27f48a761ce77f4b001bb3341d8c7f0156
Son Pham-Ba <son.phamba@epfl.ch>
2025-06-12 18:30:54 +0200

    feat: put links to external cli documentation

--- frontend/src/pages/CliPage.vue
+++ frontend/src/pages/CliPage.vue
@@ -6,48 +6,26 @@
       <p>
         To download models and components, and to setup environments to run
         those, your can use the dedicated CLI (command-line interface)
-        <a href="https://github.com/CHANGE-EPFL/frame-project-cli">tool</a>.
-      </p>
-
-      <h2>Installation</h2>
-
-      <h3>Requirements</h3>
-
-      <ul>
-        <li>
-          <a href="https://docs.astral.sh/uv/">uv</a> Python package and project
-          manager
-        </li>
-        <li><a href="https://git-scm.com/">git</a> version control system</li>
-      </ul>
-
-      <p>
-        Frame CLI relies on <a href="https://docs.astral.sh/uv/">uv</a> to
-        manage Python virtual environments. You need to install it first if you
-        don't already have it. Refer to the official
-        <a href="https://docs.astral.sh/uv/getting-started/installation/"
-          >uv documentation</a
-        >.
-      </p>
-      <p>Then, run the following command to install Frame CLI:</p>
-      <CopyCommand command="uv tool install frame-cli" />
-
-      <h2>Usage</h2>
-      <p>To see the list of available commands, run:</p>
-      <CopyCommand command="frame --help" />
-      <p>
-        Hybrid model and component pages show which command must be run to
-        download and setup specific units.
+        <a href="https://github.com/CHANGE-EPFL/frame-project-cli"></a>.
       </p>
 
       <p>
-        You may want to install autocompletion for easier usage. To do so, run:
+        Head over to the
+        <a
+          href="https://change-epfl.github.io/frame-project-cli/src/user/quick_start.html"
+          >Quick start</a
+        >
+        documentation page to install the CLI tool and start using it.
       </p>
-      <CopyCommand command="frame --install-completion" />
 
-      <h2>Uninstall</h2>
-      <p>To remove the CLI tool from your system, run the following command:</p>
-      <CopyCommand command="uv tool uninstall frame-cli" />
+      <p>
+        You can get further information on each feature of the tool in the
+        <a
+          href="https://change-epfl.github.io/frame-project-cli/src/user/user_guide.html"
+          >User guide</a
+        >
+        section.
+      </p>
     </div>
   </q-page>
 </template>
--- frontend/src/pages/ContributePage.vue
+++ frontend/src/pages/ContributePage.vue
@@ -4,14 +4,14 @@
       <h1>Add your own metadata</h1>
 
       <p>
-        You can use the <router-link to="/cli">CLI tool</router-link> to create
-        and add your own models' and components' metadata to this library. You
-        can also manually file a Pull Request on the
+        You can use the <router-link to="/cli">CLI tool</router-link> to create,
+        validate, and add your own models' and components' metadata to this
+        library. You can also manually file a Pull Request on the
         <a href="https://github.com/CHANGE-EPFL/frame-project"
           >GitHub repository</a
         >
-        of the FRAME project. Your submission will be reviewed by the project's
-        maintainers.
+        of the FRAME project. In both cases, your submission will be reviewed by
+        the project's maintainers.
       </p>
     </div>
   </q-page>
